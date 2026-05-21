4cb7ac517dffd94b80f134eb00f63c7dbdba7d14
AVERGNET <a.v.vergnet@gmail.com>
2026-05-21 16:14:46 -0700

    basic rocksdb setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use rocksdb::{Options, DB};
+
+fn main() -> Result<(), rocksdb::Error> {
+    let path = "./data/db";
+    let db = DB::open_default(path)?;
+
+    db.put(b"my-key", b"my-value")?;
+    match db.get(b"my-key")? {
+        Some(value) => println!("value: {:?}", value),
+        None => println!("key not found"),
+    }
+    Ok(())
 }
